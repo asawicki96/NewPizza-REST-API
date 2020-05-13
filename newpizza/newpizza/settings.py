091ddf345931825e2837b11913a81736c8b93504
Adam Sawicki <asawicki1996@gmail.com>
2020-05-13 16:19:33 +0200

    schema

--- newpizza/newpizza/settings.py
+++ newpizza/newpizza/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'order',
     'user',
     'rest_framework',
+    'rest_framework_swagger',
 ]
 
 
--- newpizza/newpizza/urls.py
+++ newpizza/newpizza/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
+from rest_framework.schemas.coreapi import AutoSchema
+
+schema_view = get_schema_view(title="API")
 
 urlpatterns = [
+    path('api/schema/', schema_view),
     path('admin/', admin.site.urls),
     path('api/', include('food.api.urls')),
     path('api/', include('order.api.urls')),
